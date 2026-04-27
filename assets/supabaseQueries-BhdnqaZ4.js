9409906d944ec998353f7e286bbbc725ac6867f4
Stride-dotcom <Email@stridenw.com>
2026-04-27 14:56:01 -0700

    Deploy: DT push: real error detail in toast + STRIDE LOGISTICS fallback (v16)

--- assets/supabaseQueries-BhdnqaZ4.js
+++
@@ -1,1 +0,0 @@
-import{o as e}from"./supabaseQueries-ZnYIjsmW.js";export{e as fetchClientsFromSupabase};
---
+++ assets/supabaseQueries-DFqbDDs1.js
@@ -0,0 +1,1 @@
+import{o as e}from"./supabaseQueries-CPbbtXtS.js";export{e as fetchClientsFromSupabase};
