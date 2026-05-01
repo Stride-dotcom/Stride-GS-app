d1b7276462154ecf6946d199203c3e24a1ecf483
Stride-dotcom <Email@stridenw.com>
2026-04-30 20:55:30 -0700

    Deploy: delivery: class-aware add-on billing via billing_mode

--- assets/supabaseQueries-Dh1iD_zL.js
+++
@@ -1,1 +0,0 @@
-import{o as e}from"./supabaseQueries-CSRHgzqL.js";export{e as fetchClientsFromSupabase};
---
+++ assets/supabaseQueries-vcnuUsQo.js
@@ -0,0 +1,1 @@
+import{o as e}from"./supabaseQueries-BE9YwPwG.js";export{e as fetchClientsFromSupabase};
