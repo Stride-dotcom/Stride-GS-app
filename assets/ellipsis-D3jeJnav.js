7294ff9871c1118dcd61b2ca057b9dec5f91fda5
Stride-dotcom <Email@stridenw.com>
2026-05-13 19:55:32 -0700

    Deploy: wc-cod + doc-print-css + dt-attachments (PR #409)

--- assets/ellipsis-D3jeJnav.js
+++
@@ -1,1 +0,0 @@
-import{o as e}from"./theme-B400H5CI.js";var t=e(`ellipsis`,[[`circle`,{cx:`12`,cy:`12`,r:`1`,key:`41hilf`}],[`circle`,{cx:`19`,cy:`12`,r:`1`,key:`1wjl8i`}],[`circle`,{cx:`5`,cy:`12`,r:`1`,key:`1pcz8c`}]]);export{t};
